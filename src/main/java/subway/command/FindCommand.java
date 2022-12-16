b05971efa6e5b840096165e25c9b046ede8388e2
hong-sile <gurwns9325@naver.com>
2022-12-16 19:02:54 +0900

    feat: 각 분기마다의 명령어를 저장하는 Command Enum 추가

---
+++ src/main/java/subway/command/FindCommand.java
@@ -0,0 +1,35 @@
+package subway.command;
+
+public enum FindCommand {
+    SHORTEST_DISTANCE("1"),
+    SMALLEST_TIME("2"),
+    BACK("B");
+
+    private static final String INVALID_VALUE_MESSAGE = "1, 2, B 중에 입력해주십시오.";
+    private final String code;
+
+    FindCommand(String code) {
+        this.code = code;
+    }
+
+    public static FindCommand from(String code) {
+        for (FindCommand command : FindCommand.values()) {
+            if (command.code.equals(code)) {
+                return command;
+            }
+        }
+        throw new IllegalArgumentException(INVALID_VALUE_MESSAGE);
+    }
+
+    public boolean isShortestDistance(FindCommand command) {
+        return command == SHORTEST_DISTANCE;
+    }
+
+    public boolean isSmallestTime(FindCommand command) {
+        return command == SMALLEST_TIME;
+    }
+
+    public boolean isBack(FindCommand command) {
+        return command == BACK;
+    }
+}
---
+++ src/main/java/subway/command/MainCommand.java
@@ -0,0 +1,30 @@
+package subway.command;
+
+public enum MainCommand {
+    FIND_ROUTE("1"),
+    QUIT("Q");
+
+    private static final String INVALID_VALUE_MESSAGE = "1, Q 중에 입력해주십시오.";
+    private final String code;
+
+    MainCommand(String code) {
+        this.code = code;
+    }
+
+    public static MainCommand from(String code) {
+        for (MainCommand command : MainCommand.values()) {
+            if (command.code.equals(code)) {
+                return command;
+            }
+        }
+        throw new IllegalArgumentException(INVALID_VALUE_MESSAGE);
+    }
+
+    public boolean isFindRouteCode(MainCommand command){
+        return command == FIND_ROUTE;
+    }
+
+    public boolean isQuitCode(MainCommand command){
+        return command == QUIT;
+    }
+}
